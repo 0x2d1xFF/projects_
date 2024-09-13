48ff13df2075a6a1545a0c527ac253421982aefd
Bradley Jlord <159541110+bradleyjlord@users.noreply.github.com>
2024-09-13 13:44:55 +0800

    Add files via upload

--- calcpro/main.js
+++ calcpro/main.js
@@ -1,9 +1,24 @@
-var num1 = ''
-var num2 = ''
+var num1 = '';  
+var num2 = '';  
+var currentOperation = '';
+var isSecondOperand = false;
 
-function Click (){
-}
 
+const button0 = document.querySelector('#button0');
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+const button4 = document.querySelector('#button4');
+const button5 = document.querySelector('#button5');
+const button6 = document.querySelector('#button6');
+const button7 = document.querySelector('#button7');
+const button8 = document.querySelector('#button8');
+const button9 = document.querySelector('#button9');
+const buttonAdd = document.querySelector('#buttonAdd');
+const buttonSub = document.querySelector('#buttonSub');
+const buttonMulti = document.querySelector('#buttonMulti');
+const buttonDiv = document.querySelector('#buttonDiv');
+const buttonPoint = document.querySelector('#buttonPoint');
+const buttonEquals = document.querySelector('#buttonEquals');
+const output = document.querySelector('#output');
 
-document.querySelector("#output").innerHTML = ''
-document.querySelector("#output").innerHTML 
